b9e2d926fafc1cdaab3eace2bdc8db5e7d6abb66
Connie Wong <conniehhw@gmail.com>
2024-07-17 14:07:05 -0400

    Style for screen responsiveness

--- app/resources/poke101/page.js
+++ app/resources/poke101/page.js
@@ -47,10 +47,10 @@ function poke101() {
       {/* <div className={styles.title}>
         Pokemon 101: Frequently Asked Questions
       </div> */}
-      <Accordion defaultActiveKey="0" flush>
+      <Accordion defaultActiveKey="0" flush className={styles.accordion}>
         <AccordionItem className={styles.item} eventKey="0">
           <AccordionHeader>
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
@@ -60,12 +60,12 @@ function poke101() {
             >
               <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
               <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />
-            </svg>
-            &nbsp;&nbsp;What is a Pokemon?
+            </svg> */}
+            What is a Pokemon?
           </AccordionHeader>
           <AccordionBody className={styles.clearfix}>
             <Image
-              className={styles.img}
+              className={styles.imgr}
               src="/images/pokemon-group.png"
               height={154}
               width={250}
@@ -117,7 +117,7 @@ function poke101() {
           <AccordionHeader>What are pokemon battles?</AccordionHeader>
           <AccordionBody className={styles.clearfix}>
             <Image
-              className={styles.img}
+              className={styles.imgr}
               src="/images/pokemon-battle.png"
               height={168}
               width={300}
@@ -139,7 +139,7 @@ function poke101() {
           </AccordionHeader>
           <AccordionBody className={styles.clearfix}>
             <Image
-              className={styles.img}
+              className={styles.imgr}
               src="/images/pokemon-type.png"
               height={225}
               width={400}
@@ -159,7 +159,7 @@ function poke101() {
           </AccordionHeader>
           <AccordionBody className={styles.clearfix}>
             <Image
-              className={styles.img}
+              className={styles.imgr}
               src="/images/pokemon-evol.png"
               height={297}
               width={500}
