2d7dae635a8dd4ff86a1d6aff9d2545ad70e0961
Connie Wong <conniehhw@gmail.com>
2025-04-26 15:49:17 -0400

    Fixed responsive for Pokedex and slug page

--- app/components/PokedexGrid.js
+++ app/components/PokedexGrid.js
@@ -4,7 +4,6 @@ import { useState, useEffect, useRef } from "react";
 import { PokemonCard } from "../components/PokemonCard";
 
 import styles from "../PokedexHome/pokedex.module.css";
-import { typescript } from "@/next.config";
 
 export function PokedexGrid({ allPokemons }) {
   const [searchText, setSearchText] = useState(""); // empty string
@@ -19,31 +18,6 @@ export function PokedexGrid({ allPokemons }) {
   const filteredPokemonList = searchFilter(allPokemons);
   console.log(filteredPokemonList); // log filtered/searched pokemon name
   console.log(allPokemons); // logs all fetched 20 pokemons (all info)
-
-  // testing how to merge new arrays into original
-  const animals = ["pigs", "goats", "sheep"]; // animals array
-
-  const hey = animals.push("cows", "dog"); // adds 2 elements to animals array
-
-  console.log(animals); // logs all the items in the array called 'Animals'
-  console.log(hey); // logs number of elements in 'hey' variable which contains new length of the array
-  //(5) ["pigs", "goats", "sheep", "cows", "dog"]
-  //5
-
-  animals.push("kiwi"); // adds 1 more elements to animals array
-  console.log(animals); // logs all the items in the array called 'Animals'
-  console.log(hey); // 'hey' variable contains new length of the array
-  //(6) ["pigs", "goats", "sheep", "cows", "dog", "kiwi"]
-  //6
-
-  const vegetables = ["parsnip", "potato"]; //array 1
-  const moreVegs = ["celery", "beetroot", "carrot"]; //array 2
-
-  // Merge the second array into the first one using spread syntax
-  vegetables.push(...moreVegs);
-
-  console.log(vegetables); // ['parsnip', 'potato', 'celery', 'beetroot', 'carrot']
-  console.log(moreVegs); // this array remains unchanged
 
   return (
     <>
@@ -70,56 +44,6 @@ export function PokedexGrid({ allPokemons }) {
             </button>
           </div>
         </form>
-
-        {/* <b>Test Response 1</b> */}
-        {/* <li>
-          <b>Types</b>
-          {allPokemons.map((pokemon) => {
-            console.log(pokemon.name);
-            return pokemon.types.map((type) => {
-              console.log(type.type.name);
-
-              return (
-                <>
-                  <div>
-                    <button className={styles.pill}>{pokemon.name}</button>
-                    {pokemon.types.map((type) => {
-                      return (
-                        <>
-                          <button>{type.type.name}</button>
-                        </>
-                      );
-                    })}
-
-                    <button>{type.type.name}</button>
-                  </div>
-                </>
-              );
-            });
-          })}
-        </li> */}
-
-        {/* {allPokemons.map((pokemon) => {
-          return (
-            <>
-              <button className={styles.pill}>{pokemon.name}</button>
-            </>
-          );
-        })} */}
-
-        {/* testing */}
-        {/* {allPokemons.map((pokemon) => {
-          return pokemon.moves.map((move) => {
-            console.log(pokemon.name);
-            console.log(move.move.name);
-
-            return (
-              <>
-                <button></button>
-              </>
-            );
-          });
-        })} */}
 
         <div className={styles.all_pokemon}>
           {/* //loop through each item in the api data array and create a unique card with name of each pokemon */}
@@ -151,139 +75,8 @@ export function PokedexGrid({ allPokemons }) {
             );
             // });
           })}
-
-          {/* <IndPokemonCard name="Pikachu" />
-        <IndPokemonCard />
-        <IndPokemonCard /> */}
         </div>
       </main>
     </>
   );
 }
-
-// const typeName = type.type_name;
-// let colour;
-// if (typeName === "grass") {
-//   colour = "#7AC74C";
-// } else if (typeName === "fire") {
-//   colour = "#EE8130";
-// } else if (typeName === "poison") {
-//   colour = "#A33EA1";
-// } else {
-//   colour = "green";
-// }
-
-// console.log(colour);
-
-// response1.map((array) => {
-//   array.types.map((type) => {
-//     console.log(type.type_name);
-//   });
-// });
-
-{
-  /* {response1.map((array) => {
-          return array.types.map((type) => {
-            console.log(type.type_name);
-            return (
-              <>
-                <button
-                // style={
-                //   {
-                //     background: colour,
-                //   }
-                // }
-                >
-                  {type.type_name}{" "}
-                </button>
-              </>
-            );
-          });
-        })} */
-}
-
-{
-  /* {product.map((data) => (
-          <tr key={data.id}>
-            <td>{data.name}</td>
-            <td>{data.description}</td>
-
-            {data.color.map((datas) => {
-              return (
-                <tr key={datas.id_color}>
-                  <td>{datas.color}</td>
-                  <td>{datas.quantity}</td>
-                </tr>
-              );
-            })}
-          </tr>
-        ))} */
-}
-
-{
-  /* {response.map((array) => {
-          return array.projects.map((project) => {
-            console.log(project.project_year, project.project_name);
-            return <>{project.project_name}</>;
-          });
-        })} */
-}
-
-// const pokeName = pokemon.name;
-// console.log(pokeName);
-
-// pokemon.types.map((pokemons) => {
-//   const pokeType = pokemons.type;
-//   console.log(pokeType);
-// });
-
-// return (
-//   <>
-//     <button className={styles.pill}>{pokeName}</button>
-//   </>
-// );
-
-// map through the array
-// let response = [
-//   {
-//     id: 1,
-//     organization: "org1",
-//     projects: [
-//       { project_year: "2020", project_name: "prj11" },
-//       { project_year: "2020", project_name: "prj12" },
-//     ],
-//   },
-//   {
-//     id: 2,
-//     organization: "org2",
-//     projects: [
-//       { project_year: "2021", project_name: "prj21" },
-//       { project_year: "2021", project_name: "prj21" },
-//     ],
-//   },
-// ];
-
-// response.map((array) => {
-//   array.projects.map((project) => {
-//     console.log(project.project_year, project.project_name);
-//   });
-// });
-
-// let response1 = [
-//   {
-//     id: 1,
-//     name: "weedle",
-//     types: [{ type_name: "poison" }, { type_name: "test" }],
-//   },
-//   {
-//     id: 2,
-//     name: "kakuna",
-//     types: [{ type_name: "bug" }, { type_name: "poison" }],
-//   },
-// ];
-
-// response1.map((array) => {
-//   return array.types.map((type) => {
-//     console.log(type.type_name);
-//   });
-// });
--- app/components/PokemonCard.js
+++ app/components/PokemonCard.js
@@ -125,72 +125,73 @@ export function PokemonCard({ pokemon, name, id, sprites, type }) {
           })} */}
           <h6>#{numericId}</h6>
           <h3>{name.charAt(0).toUpperCase() + name.slice(1)}</h3>
-          <div>
-            {pokemon.types.map((type) => {
-              const typeName = type.type.name;
-              console.log(typeName);
-              // console.log(`${type}`);
+        </div>
 
-              const test = typeName;
-              let colour;
-              if (test === "grass") {
-                colour = "#7AC74C";
-              } else if (test === "fire") {
-                colour = "#EE8130";
-              } else if (test === "poison") {
-                colour = "#A33EA1";
-              } else if (test === "normal") {
-                colour = "#A8A77A";
-              } else if (test === "electric") {
-                colour = "#F7D02C";
-              } else if (test === "dragon") {
-                colour = "#6F35FC";
-              } else if (test === "ice") {
-                colour = "#96D9D6";
-              } else if (test === "fighting") {
-                colour = "C22E28";
-              } else if (test === "ground") {
-                colour = "#E2BF65";
-              } else if (test === "psychic") {
-                colour = "#F95587";
-              } else if (test === "bug") {
-                colour = "#A6B91A";
-              } else if (test === "flying") {
-                colour = "#A98FF3";
-              } else if (test === "rock") {
-                colour = "#B6A136";
-              } else if (test === "water") {
-                colour = "#6390F0";
-              } else if (test === "ghost") {
-                colour = "#735797";
-              } else if (test === "dark") {
-                colour = "#705746";
-              } else if (test === "steel") {
-                colour = "#B7B7CE";
-              } else if (test === "fairy") {
-                colour = "#D685AD";
-              } else {
-                colour = "black";
-              }
+        <div className={styles.type}>
+          {pokemon.types.map((type) => {
+            const typeName = type.type.name;
+            console.log(typeName);
+            // console.log(`${type}`);
 
-              return (
-                <>
-                  <button
-                    style={{
-                      // background: pokeColor,
-                      backgroundColor: colour,
-                      color: "white",
-                      height: "30px",
-                    }}
-                    className={styles.pill}
-                  >
-                    {/* {type.type.name} */}
-                    {typeName}
-                  </button>
-                </>
-              );
-            })}
-          </div>
+            const test = typeName;
+            let colour;
+            if (test === "grass") {
+              colour = "#7AC74C";
+            } else if (test === "fire") {
+              colour = "#EE8130";
+            } else if (test === "poison") {
+              colour = "#A33EA1";
+            } else if (test === "normal") {
+              colour = "#A8A77A";
+            } else if (test === "electric") {
+              colour = "#F7D02C";
+            } else if (test === "dragon") {
+              colour = "#6F35FC";
+            } else if (test === "ice") {
+              colour = "#96D9D6";
+            } else if (test === "fighting") {
+              colour = "C22E28";
+            } else if (test === "ground") {
+              colour = "#E2BF65";
+            } else if (test === "psychic") {
+              colour = "#F95587";
+            } else if (test === "bug") {
+              colour = "#A6B91A";
+            } else if (test === "flying") {
+              colour = "#A98FF3";
+            } else if (test === "rock") {
+              colour = "#B6A136";
+            } else if (test === "water") {
+              colour = "#6390F0";
+            } else if (test === "ghost") {
+              colour = "#735797";
+            } else if (test === "dark") {
+              colour = "#705746";
+            } else if (test === "steel") {
+              colour = "#B7B7CE";
+            } else if (test === "fairy") {
+              colour = "#D685AD";
+            } else {
+              colour = "black";
+            }
+
+            return (
+              <>
+                <button
+                  style={{
+                    // background: pokeColor,
+                    backgroundColor: colour,
+                    color: "white",
+                    height: "30px",
+                  }}
+                  className={styles.pill}
+                >
+                  {/* {type.type.name} */}
+                  {typeName}
+                </button>
+              </>
+            );
+          })}
         </div>
 
         {/* {pokemonStats.name.charAt(0).toUpperCase() +
