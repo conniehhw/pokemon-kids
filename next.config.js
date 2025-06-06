cfba9b957ac5499004e9a76a902a2e8c7fe756a2
Connie Wong <conniehhw@gmail.com>
2025-06-06 00:05:18 -0400

    deploy changes to next version to 13.5.2 to fix image error

--- next.config.js
+++ next.config.js
@@ -1,18 +1,3 @@
-/** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["cdn.coinranking.com"],
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "raw.githubusercontent.com",
-//         port: "",
-//         pathname: "/PokeAPI/sprites/master/sprites/pokemon/**",
-//       },
-//     ],
-//   },
-// };
-
 module.exports = {
   // reactStrictMode: false,
   images: {
